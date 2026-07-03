f10b6bc20a980c0a9e0608d2ef19af8902772c1c
ScarFace <nitingupta4592@gmail.com>
2026-07-03 16:49:37 +0530

    Refactor comments in utils.py for clarity and consistency

--- backend/utils.py
+++ backend/utils.py
@@ -1,15 +1,18 @@
-import fitz
+import fitz #reads pdf it is PyMuPDF
 import faiss
-import numpy as np
+import numpy as np #to pass vector to faiss
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+#this is for only one resume
 index = None
 documents = []
 resume_text = ""
 
+#everything here is a function becoz every files need them
 
+# extracts text from pdf and returns it as string sexy function
 def extract_text(pdf_path):
     text = ""
 
@@ -31,7 +34,8 @@ def chunk_text(text, chunk_size=500):
 
     return chunks
 
-
+#sbse jaruri function, it creates vector store from the resume text and stores it in faiss index
+#this function calls only once, by upload.py
 def create_vector_store(text):
 
     global index
@@ -41,13 +45,15 @@ def create_vector_store(text):
     resume_text = text
 
     documents = chunk_text(text)
-
+    
+    
+#step 4:where AI starts
     embeddings = model.encode(documents)
 
     dimension = embeddings.shape[1]
 
     index = faiss.IndexFlatL2(dimension)
-
+#faiss stores vector in flat32
     index.add(np.array(embeddings).astype("float32"))
 
 
@@ -55,7 +61,7 @@ def retrieve(query, k=3):
 
     global index
 
-    if index is None:
+    if index is None: #if user not uploads any resume and ask question, then empty string will be returned
         return ""
 
     query_embedding = model.encode([query])
